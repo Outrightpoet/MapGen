5f9f2721eae5323edb838cdda65efc8c86164594
outri <outrightpoet@gmail.com>
2025-12-24 16:40:36 -0600

    fleshed out

--- plots.py
+++ plots.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap, BoundaryNorm
+from matplotlib.colors import ListedColormap
 import numpy as np
 
 def plt_data(data1, data2, data3, data4, data5, data6):
@@ -20,25 +20,28 @@ def plt_data(data1, data2, data3, data4, data5, data6):
     axs[2, 0].imshow(data5, cmap='RdBu_r', interpolation='nearest')
     axs[2, 0].title.set_text('Tempature')
 
-    BIOME_COLORS = {
-        0: "#003366",  # ocean (deep blue)
-        1: "#3366cc",  # lake (blue)
-        2: "#2e8b57",  # swamp (dark green)
-        3: "#edc9af",  # desert (sand)
-        4: "#7cfc00",  # grassland (bright green)
-        5: "#228b22",  # temperate forest (forest green)
-        6: "#006400",  # rainforest (deep green)
-        7: "#2f4f4f",  # taiga (cold dark green)
-        8: "#e0f8ff",  # tundra (icy blue-white)
-        9: "#8b7765",  # mountain (rock brown)
-        10: "#9acd32",  # alpine (yellow-green)
-        11: "#aaaaaa",  # rocky peak (gray)
-        12: "#b8860b",  # badlands (dry brown)
-        13: "#556b2f",  # farmland (olive)
-    }
+    BIOME_COLORS = [
+        "#003366",  # 0 ocean (deep blue)
+        "#3366cc",  # 1 lake (blue)
+        "#2e8b57",  # 2 swamp (dark green)
+        "#edc9af",  # 3 desert (sand)
+        "#7cfc00",  # 4 grassland (bright green)
+        "#228b22",  # 5 temperate forest (forest green)
+        "#006400",  # 6 rainforest (deep green)
+        "#2f4f4f",  # 7 taiga (cold dark green)
+        "#e0f8ff",  # 8 tundra (icy blue-white)
+        "#8b7765",  # 9 mountain (rock brown)
+        "#9acd32",  # 10 alpine (yellow-green)
+        "#aaaaaa",  # 11 rocky peak (gray)
+        "#b8860b",  # 12 badlands (dry brown)
+        "#556b2f",  # 13 farmland (olive)
+    ]
 
-    axs[2, 1].imshow(data6, cmap=BIOME_COLORS, interpolation='nearest')
-    axs[2, 2].title.set_text('Biomes')
+    cmap = ListedColormap(BIOME_COLORS)
+    axs[2, 1].imshow(data5, cmap=cmap, interpolation='nearest')
+
+    axs[2, 1].imshow(data6, cmap=cmap, interpolation='nearest')
+    axs[2, 1].title.set_text('Biomes')
 
     plt.tight_layout()
     plt.show()
